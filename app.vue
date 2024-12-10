51cb9c51c415c345a931ea6b44cf7471c3ebfca4
stianak <stianakdev@gmail.com>
2024-12-10 14:29:16 +0100

    add code causing errors

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <ButtonComponent disabled />
   </div>
 </template>
---
+++ components/ButtonComponent.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// import type { ButtonProps } from "@nuxt/ui";
+// import type { ButtonProps } from "@nuxt/ui/runtime/components/Button.vue";
+import type { ButtonProps } from "#ui/types";
+import { defineProps } from "vue";
+
+const props = defineProps<ButtonProps>();
+</script>
+
+<template>
+  <UButton v-bind="props">Test</UButton>
+</template>
